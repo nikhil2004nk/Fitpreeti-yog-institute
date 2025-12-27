30b505a69223e5868fd0143f6b8c541f5fa63810
nikhil2004nk <nikhil2004nk@gmail.com>
2025-12-27 19:18:46 +0530

    fixed map address and routing

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -177,8 +177,8 @@ export const Contact: React.FC = () => {
                         <span className="sr-only">Facebook</span>
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                           <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
-                        </svg>
-                      </a>
+                      </svg>
+                    </a>
                     )}
                     {social?.youtube && (
                       <a
@@ -190,8 +190,8 @@ export const Contact: React.FC = () => {
                         <span className="sr-only">YouTube</span>
                         <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                           <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-                        </svg>
-                      </a>
+                      </svg>
+                    </a>
                     )}
                   </div>
                 </div>
@@ -202,14 +202,15 @@ export const Contact: React.FC = () => {
                 <h3 className="text-xl font-bold text-gray-900 mb-4">Find Us</h3>
                 <div className="aspect-w-16 aspect-h-9 rounded-xl overflow-hidden">
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3480.1234567890123!2d76.12345678901234!3d29.123456789012345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjnCsDA3JzI0LjQiTiA3NsKwMDcnMjQuMCJF!5e0!3m2!1sen!2sin!4v1234567890123!5m2!1sen!2sin"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.3189694587385!2d72.83786887499297!3d19.204596182040706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b7c9a9e8a8b5%3A0x5c7e3e3e3e3e3e3e!2sD-8%2C%20Soc%20No.30%2C%20Kandivali%2C%20Nilkanth%20Nagar%2C%20Ganesh%20Nagar%2C%20Kandivali%20West%2C%20Mumbai%2C%20Maharashtra%20400067!5e0!3m2!1sen!2sin!4v1640000000000"
                     width="100%"
-                    height="300"
+                    height="400"
                     style={{ border: 0 }}
                     allowFullScreen
                     loading="lazy"
                     className="rounded-lg"
                     title="FitPreeti Yog Institute Location"
+                    referrerPolicy="no-referrer-when-downgrade"
                   ></iframe>
                 </div>
               </div>
